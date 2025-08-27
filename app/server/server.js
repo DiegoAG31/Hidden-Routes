df4913acc1fcbbe9acb576aa2b7496fc2556ad46
DiegoAG31 <diegoag3107@gmail.com>
2025-08-26 19:59:14 -0500

    update type module ES

--- app/server/server.js
+++ app/server/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const authRoutes = require('./routes/auth.js');
-const bookingRoutes = require('./routes/booking.js');
-const experienceRoutes = require('./routes/experience.js');
-const reviewRoutes = require('./routes/review.js');
-const sequelize = require('./database.js');
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/auth.js';
+// import bookingRoutes from './routes/bookings.js';
+// import experienceRoutes from './routes/experiences.js';
+// import reviewRoutes from './routes/reviews.js';
+import sequelize from './database.js';
 
 const app = express();
 
@@ -12,19 +12,19 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-app.use('/api/bookings', bookingRoutes);
-app.use('/api/experiences', experienceRoutes);
-app.use('/api/reviews', reviewRoutes);
+// app.use('/api/bookings', bookingRoutes);
+// app.use('/api/experiences', experienceRoutes);
+// app.use('/api/reviews', reviewRoutes);
 
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
-    console.log(`Server is running on port ${PORT}`);
-    try {
-        await sequelize.authenticate();
-        console.log('Database connected!');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+  console.log(`Server is running on port ${PORT}`);
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected!');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 });
