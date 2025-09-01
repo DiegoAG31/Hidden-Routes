a009478a4f2c423756c8c985a1b72de48cc423d0
DiegoAG31 <diegoag3107@gmail.com>
2025-08-31 21:01:33 -0500

    variables db config

--- app/server/database.js
+++ app/server/database.js
@@ -2,6 +2,13 @@ import { Sequelize } from 'sequelize';
 import config from '../config/config.js';
 
 const env = process.env.NODE_ENV || 'development';
-const sequelize = new Sequelize(config[env]);
+
+let sequelize;
+
+if (config[env].use_env_variable) {
+  sequelize = new Sequelize(process.env[config[env].use_env_variable], config[env]);
+} else {
+  sequelize = new Sequelize(config[env]);
+}
 
 export default sequelize;
